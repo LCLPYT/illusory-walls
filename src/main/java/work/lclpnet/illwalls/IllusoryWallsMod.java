19e1c37aa85e7250004dc296fb580a5c699a2b6f
LCLP <lukas@lclpnet.work>
2023-04-09 23:12:04 +0200

    move staff of illusion functionality to item class

--- src/main/java/work/lclpnet/illwalls/IllusoryWallsMod.java
+++ src/main/java/work/lclpnet/illwalls/IllusoryWallsMod.java
@@ -2,34 +2,27 @@ package work.lclpnet.illwalls;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.entity.SpawnReason;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.particle.DustParticleEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import work.lclpnet.illwalls.entity.IllusoryWallEntity;
 import work.lclpnet.illwalls.entity.StructureEntity;
 import work.lclpnet.illwalls.item.StaffOfIllusionItem;
 import work.lclpnet.illwalls.wall.IllusoryWallLookup;
+import work.lclpnet.illwalls.wall.IllusoryWallManager;
 import work.lclpnet.illwalls.wall.NaiveWallLookup;
 import work.lclpnet.kibu.schematic.SchematicFormats;
 import work.lclpnet.kibu.schematic.api.SchematicFormat;
@@ -65,7 +58,13 @@ public class IllusoryWallsMod implements ModInitializer, IllusoryWallsApi {
     );
     public static final SchematicFormat SCHEMATIC_FORMAT = SchematicFormats.SPONGE_V2;
     private static IllusoryWallsMod instance = null;
-    private final IllusoryWallLookup wallLookup = new NaiveWallLookup();
+    private final IllusoryWallLookup wallLookup;
+    private final IllusoryWallManager wallManager;
+
+    public IllusoryWallsMod() {
+        this.wallLookup = new NaiveWallLookup();
+        this.wallManager = new IllusoryWallManager(wallLookup);
+    }
 
     public static Identifier identifier(String path) {
         return new Identifier(MOD_ID, path);
@@ -92,76 +91,18 @@ public class IllusoryWallsMod implements ModInitializer, IllusoryWallsApi {
             entries.add(STAFF_OF_ILLUSION_ITEM);
         });
 
-        LOGGER.info("Initialized.");
+        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+            if (world.isClient || hand != Hand.MAIN_HAND) return ActionResult.PASS;
 
-        test();
+            boolean success = wallManager.fadeWallAtIfPresent((ServerWorld) world, pos);
+            return success ? ActionResult.CONSUME : ActionResult.PASS;
+        });
+
+        LOGGER.info("Initialized.");
     }
 
     @Override
     public IllusoryWallLookup lookup() {
         return wallLookup;
-    }
-
-    private void test() {
-        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-            if (world.isClient) return ActionResult.PASS;
-            if (hand != Hand.MAIN_HAND || !player.getStackInHand(hand).isOf(STAFF_OF_ILLUSION_ITEM))
-                return ActionResult.PASS;
-            if (hitResult.getType() != HitResult.Type.BLOCK) return ActionResult.PASS;
-
-            final var pos = hitResult.getBlockPos();
-            final var serverWorld = (ServerWorld) world;
-
-            if (wallLookup.getWallAt(serverWorld, pos).isPresent())
-                return ActionResult.PASS;  // there is already a wall
-
-            // check neighbours
-            IllusoryWallEntity entity = null;
-            var adjPos = new BlockPos.Mutable();
-
-            for (Direction direction : Direction.values()) {
-                adjPos.set(pos, direction);
-
-                var opt = wallLookup.getWallAt(serverWorld, adjPos);
-                if (opt.isEmpty()) continue;
-
-                // found an adjacent wall
-                entity = opt.get();
-                break;
-            }
-
-            final ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-
-            if (entity == null) {
-                IllusoryWallsMod.ILLUSORY_WALL_ENTITY.spawn(serverWorld, null, wall -> updateEntity(serverWorld, pos, wall, serverPlayer), pos, SpawnReason.SPAWN_EGG, false, false);
-            } else {
-                updateEntity(serverWorld, pos, entity, serverPlayer);
-            }
-
-            return ActionResult.CONSUME;
-        });
-
-        AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
-            if (world.isClient || hand != Hand.MAIN_HAND) return ActionResult.PASS;
-
-            ServerWorld serverWorld = (ServerWorld) world;
-            var optWall = wallLookup.getWallAt(serverWorld, pos);
-            if (optWall.isEmpty()) return ActionResult.PASS;
-
-            var entity = optWall.get();
-            entity.fade();
-
-            return ActionResult.CONSUME;
-        });
-    }
-
-    private void updateEntity(ServerWorld world, BlockPos pos, IllusoryWallEntity entity, ServerPlayerEntity invoker) {
-        var structure = entity.getStructureContainer().getWrapper();
-        structure.setBlockState(pos, world.getBlockState(pos));
-
-        Vec3d center = pos.toCenterPos();
-
-        world.spawnParticles(invoker, new DustParticleEffect(Vec3d.unpackRgb(0x770077).toVector3f(), 0.6f),
-                false, center.x, center.y, center.z, 100, 0.5f, 0.5f, 0.5f, 0.1);
     }
 }
--- src/main/java/work/lclpnet/illwalls/item/StaffOfIllusionItem.java
+++ src/main/java/work/lclpnet/illwalls/item/StaffOfIllusionItem.java
@@ -1,7 +1,22 @@
 package work.lclpnet.illwalls.item;
 
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.particle.DustParticleEffect;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import work.lclpnet.illwalls.IllusoryWallsApi;
+import work.lclpnet.illwalls.IllusoryWallsMod;
+import work.lclpnet.illwalls.entity.IllusoryWallEntity;
+import work.lclpnet.illwalls.wall.IllusoryWallLookup;
 
 public class StaffOfIllusionItem extends Item {
 
@@ -12,5 +27,60 @@ public class StaffOfIllusionItem extends Item {
     @Override
     public boolean hasGlint(ItemStack stack) {
         return true;
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        PlayerEntity player = context.getPlayer();
+        World world = context.getWorld();
+        BlockPos pos = context.getBlockPos();
+
+        if (!world.isClient && player != null && !use((ServerPlayerEntity) player, (ServerWorld) world, pos)) {
+            return ActionResult.FAIL;
+        }
+
+        return ActionResult.success(world.isClient);
+    }
+
+    private boolean use(ServerPlayerEntity player, ServerWorld world, BlockPos pos) {
+        if (!player.isCreativeLevelTwoOp()) return false;
+
+        IllusoryWallLookup wallLookup = IllusoryWallsApi.getInstance().lookup();
+
+        if (wallLookup.getWallAt(world, pos).isPresent())
+            return false;  // there is already a wall
+
+        // check neighbours
+        IllusoryWallEntity entity = null;
+        var adjPos = new BlockPos.Mutable();
+
+        for (Direction direction : Direction.values()) {
+            adjPos.set(pos, direction);
+
+            var opt = wallLookup.getWallAt(world, adjPos);
+            if (opt.isEmpty()) continue;
+
+            // found an adjacent wall
+            entity = opt.get();
+            break;
+        }
+
+        if (entity == null) {
+            IllusoryWallsMod.ILLUSORY_WALL_ENTITY.spawn(world, null, wall -> updateEntity(world, pos, wall, player), pos, SpawnReason.SPAWN_EGG, false, false);
+        } else {
+            updateEntity(world, pos, entity, player);
+        }
+
+        return true;
+    }
+
+    private void updateEntity(ServerWorld world, BlockPos pos, IllusoryWallEntity entity, ServerPlayerEntity invoker) {
+        var structure = entity.getStructureContainer().getWrapper();
+        structure.setBlockState(pos, world.getBlockState(pos));
+
+        Vec3d center = pos.toCenterPos();
+
+        world.spawnParticles(invoker, new DustParticleEffect(Vec3d.unpackRgb(0x770077).toVector3f(), 0.6f),
+                false, center.x, center.y, center.z, 100, 0.5f, 0.5f, 0.5f, 0.1);
     }
 }
